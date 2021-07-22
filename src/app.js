8b6f93999241239cb42c698d39e62e2ca7fd0062
Pyrex <retojonatan@gmail.com>
2021-07-22 12:57:38 -0300

    added app with routes and settings

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
-const usersRoute = require("./routes/users");
+const exphbs = require("express-handlebars");
+const config = require("./config");
+const path = require("path");
+const authRoutes = require("./routes/auth.routes");
+const appRoutes = require("./routes/app.routes");
 
 const app = express();
 
+app.set("PORT", config.PORT || 3000);
+app.set("secret", config.SK);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partials: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", ".hbs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -11,10 +33,10 @@ app.use(
   })
 );
 
-app.use(cors());
-
-app.use("/api/users", usersRoute);
-
-app.set("port", process.env.PORT || 3000);
+app.use("/api", authRoutes);
+app.use("/api", appRoutes);
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 module.exports = app;
